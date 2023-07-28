b4c6e89204390fa458d8b9e63cc622d869020cf6
jefferson <jeffer4533@gmail.com>
2023-07-27 19:29:05 -0500

    Solucionar estilos categorias

--- pages/book/[id].tsx
+++ pages/book/[id].tsx
@@ -67,7 +67,7 @@ const Book: React.FC<BookType> = (props) => {
             <div className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
               <ReactMarkdown children={description} />
             </div>
-            <div>
+            <div className="flex gap-2 flex-wrap max-w-sm">
               {categories.map((item) => {
                 return (
                   <button
